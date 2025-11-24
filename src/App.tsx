5a355fe97438d58ceb199068840f705d48bf1bd2
Raycel-create <ctinamanubay@gmail.com>
2025-11-24 10:52:23 +0000

    Edited Spark

--- src/App.tsx
+++ src/App.tsx
@@ -36,8 +36,6 @@ function AppContent() {
         return <Generator onNavigate={setCurrentPage} />
       case 'auth':
         return <AuthLanding onNavigate={setCurrentPage} />
-      case 'figma':
-        return <FigmaIntegration onNavigate={setCurrentPage} />
       default:
         return <Home onNavigate={setCurrentPage} />
     }
